462764435483389f3a53d987e36bc2ea95082288
laurenfb <laurenfriesbrink@gmail.com>
2017-03-30 15:31:53 -0700

    vesioning with custom comparator\!

---
+++ foobar/versioning.py
@@ -0,0 +1,38 @@
+# Inputs:
+INPUT1 = ["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"]
+# Output:
+OUTPUT2 =["1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"]
+#
+# Inputs:
+INPUT = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
+# Output:
+OUTPUT =["0.1", "1.1.1", "1.2", "1.2.1", "1.11", "2", "2.0", "2.0.0"]
+
+def answer(l):
+    l = sorted(l)
+    print "first sorted l" + str(l)
+    l = sorted(l, cmp=custom_cmp)
+    print "second sorted l" + str(l)
+
+def custom_cmp(a,b):
+    "custom cmp"
+    # print a
+    # print b
+    a = a.split('.')
+    b = b.split('.')
+    if a[0] == b[0]:
+        return 0
+    else:
+        if len(a) == 1 or len(b) == 1:
+            return 0
+        if a[1] > b[1]:
+            return -1
+        elif a[1] == b[1]:
+            return 0
+        else:
+            return -1
+
+
+
+answer(INPUT)
+print OUTPUT
